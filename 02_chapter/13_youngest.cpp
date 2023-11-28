26064744b8d8d60847537d8ac716aa0f361d2bab
unknow <t0rjan@kali.unknow>
2023-11-28 17:12:01 +0530

    code correction in 13_youngest and complete the code in the 14_grade files

--- 02_chapter/13_youngest.cpp
+++ 02_chapter/13_youngest.cpp
@@ -3,28 +3,28 @@ using namespace std;
 int main(){
     // HERE THE RAM IS X AND SHYAM IS Y AND THE AJAY IS Z
     int x ;
-    cout<<"Enter your first number here :";
+    cout<<"Enter your first age here :";
     cin>>x;
     int y ;
-    cout<<"Enter your second number here :";
+    cout<<"Enter your second age here :";
     cin>>y;
     int z ;
-    cout<<"Enter your third here :";
+    cout<<"Enter your third age here :";
     cin>>z;
     if(x<y){
         if(x<z){
-            cout<<"The graetest number is :"<<x;
+            cout<<"The youngest age is :"<<x;
         }
         else
-            cout<<"The greatest number is :"<<z;
+            cout<<"The youngest age is :"<<z;
     }
     else{
         if(y<z){
-            cout<<"The greatest number is :"<<y;
+            cout<<"The youngest age is :"<<y;
         }
         else{
-            cout<<"The greatest number is :"<<z;
+            cout<<"The youngest age is :"<<z;
         }
 
     }
-}
+}   
